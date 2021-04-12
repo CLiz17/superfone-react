60c595cf338777332048fc042186732f348a4505
Cliz17 <liz362002@gmail.com>
2021-04-12 17:47:31 +0530

    updated

--- src/pages/Integrations.jsx
+++ src/pages/Integrations.jsx
@@ -8,17 +8,15 @@ const Integration = () => {
         <div className="row">
           <div className="col-10 mx-auto">
           <div className="row">
-            <div className="col-md-6 pt-5 pt-lg-0 order-2 order-lg-1 
+            <div className="col-md-6 pt-5 pt-lg-0 order-1 order-lg-2
                         d-flex justify-content-center flex-column">
-              <h1><p style="color:#3059c8">Superfone Integrates<br/>Seamlessly With All Leading <br/>CRMs Used By Sales Teams</h1></p>
+              <h1>Superfone Integrates<br/>Seamlessly With All Leading <br/>CRMs Used By Sales Teams</h1>
            <h5>Easily keep your CRM updated in real-time with all<br/>customer calls and notes,
-              tasks so you always have a <b/>complete picture of your pipeline</h5> 
-            <div className="mt-3 button-row">
-            <a href="" type="button" className="btn1 btn-outline-primary
-                   btn-lg signin-button">Request Demo</a>
+              tasks so you always have a <br/>complete picture of your pipeline</h5> 
+            <div className="mt-3">
+            <a href="" type="button" className="btn1 btn-outline-primary btn-lg ">Request Demo</a>
 
-                  <a href="" type="button" className="btn btn-primary btn-lg 
-                  trial-button">Start Free Trial</a>
+            <a href="" type="button" className="btn btn-primary btn-lg ">Start Free Trial</a>
             </div>
             </div>
             <div className="col-lg-6 order-1 order-lg-2 header-img">
