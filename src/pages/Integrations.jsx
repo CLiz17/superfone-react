d5e0c029f6bc6bbea63ecf7a6a495e632156467e
CLiz17 <liz362002@gmail.com>
2021-04-23 10:53:12 +0530

    updated

--- src/pages/Integrations.jsx
+++ src/pages/Integrations.jsx
@@ -3,30 +3,166 @@ import Side from "./illustration.png"
 
 const Integration = () => {
   return(
-    <section id="header" className="d-flex align-items-center">
-      <div className="container-fluid nav_bg">
+    <>
+         <section id="header" className="d-flex align-items-center">
+      <div className="container-fluid nav_bg mb-5">
         <div className="row">
           <div className="col-10 mx-auto">
           <div className="row">
-            <div className="col-md-6 pt-5 pt-lg-0 order-1 order-lg-2
-                        d-flex justify-content-center flex-column head-text">
-              <h1 className="main-header">Superfone Integrates<br/>Seamlessly With All Leading <br/>CRMs Used By Sales Teams<br/></h1>
-           <h5 className="main-body">Easily keep your CRM updated in real-time with all<br/>customer calls and notes,
-              tasks so you always have a <br/>complete picture of your pipeline</h5> 
-            <div className="mt-3">
-            <a href="" type="button" className="btn1 btn-outline-primary btn-lg ">Request Demo</a>
 
-            <a href="" type="button" className="btn btn-primary btn-lg ">Start Free Trial</a>
-            </div>
-            </div>
-            <div className="col-lg-6 order-1 order-lg-2 header-img">
+               <div className="col-md-6 pt-5 pt-lg-0 order-1 order-lg-2
+                        d-flex justify-content-center flex-column head-text">
+                      <h1 className="main-header">Superfone Integrates<br/>Seamlessly With All Leading <br/>CRMs Used By Sales Teams<br/></h1>
+                     <h5 className="main-body">Easily keep your CRM updated in real-time with all<br/>customer calls and notes,
+                                        tasks so you always have a <br/>complete picture of your pipeline</h5> 
+                 <div className="mt-3">
+                    <a href="" type="button" className="btn1 btn-outline-primary btn-lg ">Request Demo</a>
+
+                     <a href="" type="button" className="btn btn-primary btn-lg ">Start Free Trial</a>
+                 </div>
+              </div>
+
+              <div className="col-lg-6 order-1 order-lg-2 header-img">
                   <img src={Side} className="img-fluid animated" alt="illustration png" />
-            </div>
+              </div>
+
             </div>
           </div>
         </div>
+        </div>
+      </section>
+
+  <div class="card text-center">
+  <div class="card-header">
+    <ul class="nav nav-tabs card-header-tabs">
+      <li class="nav-item">
+        <a class="nav-link active" aria-current="true" href="#">Active</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Link</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+      </li>
+    </ul>
+  </div>
+  <div class="card">
+        <div class="card-body">
+        <div className="container-fluid mb-5">
+     <div className="row">
+      <div className="col-10 mx-auto">
+        <div className="row">
+           
+            <div className="col-md-4 col-10 mx-auto">
+            <div class="card">
+                <img src="..." class="card-img-top" alt="..."/>
+
+                  <div class="card-body">
+                      <h5 class="card-title">Card title</h5>
+                      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                     <a href="#" class="btn btn-primary">Go somewhere</a>
+                 </div>
+             </div>
+           </div>
+
+      <div className="col-md-4 col-10 mx-auto">
+        <div class="card">
+        <img src="..." class="card-img-top" alt="..."/>
+         <div class="card-body">
+         <h5 class="card-title">Card title</h5>
+           <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+           <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
       </div>
-    </section>
+        </div>
+
+      <div className="col-md-4 col-10 mx-auto">
+        <div class="card">
+        <img src="..." class="card-img-top" alt="..."/>
+         <div class="card-body">
+         <h5 class="card-title">Card title</h5>
+           <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+           <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
+      </div>
+        </div>
+
+      <div className="col-md-4 col-10 mx-auto">
+        <div class="card">
+        <img src="..." class="card-img-top" alt="..."/>
+         <div class="card-body">
+         <h5 class="card-title">Card title</h5>
+           <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+           <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
+      </div>
+        </div>
+
+      <div className="col-md-4 col-10 mx-auto">
+        <div class="card">
+        <img src="..." class="card-img-top" alt="..."/>
+         <div class="card-body">
+         <h5 class="card-title">Card title</h5>
+           <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+           <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
+      </div>
+        </div>
+
+    <div className="col-md-4 col-10 mx-auto">
+      <div class="card">
+        <img src="..." class="card-img-top" alt="..."/>
+        <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+         <a href="#" class="btn btn-primary">Go somewhere</a>
+      </div>
+    </div>
+  </div>
+
+  <div className="col-md-4 col-10 mx-auto">
+      <div class="card">
+        <img src="..." class="card-img-top" alt="..."/>
+        <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+         <a href="#" class="btn btn-primary">Go somewhere</a>
+      </div>
+    </div>
+  </div>
+
+  <div className="col-md-4 col-10 mx-auto">
+      <div class="card">
+        <img src="..." class="card-img-top" alt="..."/>
+        <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+         <a href="#" class="btn btn-primary">Go somewhere</a>
+      </div>
+    </div>
+  </div>
+  </div>
+  </div>
+  </div>
+  </div>  
+  </div>
+</div> 
+</div>
+
+  <div class="card text-center">
+  <div class="card-header">
+    Featured
+  </div>
+  <div class="card-body">
+    <h5 class="card-title">Special title treatment</h5>
+    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+  <div class="card-footer text-muted">
+    2 days ago~
+  </div>
+</div>
+  </>
   )
 } 
 
