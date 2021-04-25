219323fd9842a3eafb3c6c1a8a7d30ab455ab90b
CLiz17 <liz362002@gmail.com>
2021-04-25 07:51:24 +0530

    updated

--- src/pages/Integrations.jsx
+++ src/pages/Integrations.jsx
@@ -12,7 +12,7 @@ const Integration = () => {
 
                <div className="col-md-6 pt-5 pt-lg-0 order-1 order-lg-2
                         d-flex justify-content-center flex-column head-text">
-                      <h1 className="main-header">Superfone Integrates<br/>Seamlessly With All Leading <br/>CRMs Used By Sales Teams<br/></h1>
+                      <h1 className="main-header">Superfone Integrates<br/>Seamlessly With All Leading <br/>CRMs Used By Sales Teams</h1>
                      <h5 className="main-body">Easily keep your CRM updated in real-time with all<br/>customer calls and notes,
                                         tasks so you always have a <br/>complete picture of your pipeline</h5> 
                  <div className="mt-3">
@@ -31,21 +31,37 @@ const Integration = () => {
         </div>
         </div>
       </section>
-
-  <div class="card text-center">
-  <div class="card-header">
-    <ul class="nav nav-tabs card-header-tabs">
-      <li class="nav-item">
-        <a class="nav-link active">Active</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link disabled">Link</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link disabled">Disabled</a>
-      </li>
-    </ul>
+<br/>
+<br/>
+<br/>
+<br/>
+<br/>
+<br/>
+<br/>
+   
+     <div className="body">
+      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container-fluid">
+    <a class="navbar-brand" styles={"color: #fefffe"}><b>CRMs</b></a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" >Calanders</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" >Dialers</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link">Dialers</a>
+        </li>
+      </ul>
+    </div>
   </div>
+</nav>
+
   <div class="card">
         <div class="card-body">
         <div className="container-fluid mb-5">
@@ -53,20 +69,20 @@ const Integration = () => {
       <div className="col-10 mx-auto">
         <div className="row">
            
-            <div className="col-md-4 col-10 mx-auto">
+            <div className="col-md-4 col-10 mx-auto" styles={"width: 350px;height: 350px"}>
             <div class="card">
                 <img src="https://raw.githubusercontent.com/CLiz17/superfone-integ/main/Images/hubspot.png" class="card-img-top"/>
                       <hr/>
                   <div class="card-body">
-                      <p class="card-text">HubSpot is one of the most popular CRM tools.
-                       With a one-click sync, your HubSpot information will appear in
-                        Wingman (chosen fields) and information will be pushed to your
+                      <p class="card-text">HubSpot is one of the most popular CRM <br/> tools.
+                       With a one-click sync, your HubSpot<br/> information will appear in
+                        Wingman<br/> (chosen fields) and information will be<br/> pushed to your
                          CRM.</p>
                  </div>
              </div>
            </div>
 
-      <div className="col-md-4 col-10 mx-auto">
+      <div className="col-md-4 col-10 mx-auto cards">
         <div class="card">
         <img  src="https://raw.githubusercontent.com/CLiz17/superfone-integ/main/Images/zoho.png" class="card-img-top"/>
         <hr/>
@@ -79,7 +95,7 @@ const Integration = () => {
       </div>
         </div>
 
-      <div className="col-md-4 col-10 mx-auto">
+      <div className="col-md-4 col-10 mx-auto cards" styles={"box-shadow: 15px 30px"}>
         <div class="card">
         <img src="https://raw.githubusercontent.com/CLiz17/superfone-integ/main/Images/salesforce.png" class="card-img-top" />
          <div class="card-body">
@@ -92,7 +108,7 @@ const Integration = () => {
       </div>
         </div>
 
-      <div className="col-md-4 col-10 mx-auto">
+      <div className="col-md-4 col-10 mx-auto cards">
         <div class="card">
         <img src="https://raw.githubusercontent.com/CLiz17/superfone-integ/main/Images/microsoft.png" class="card-img-top" />
          <div class="card-body">
@@ -106,7 +122,7 @@ const Integration = () => {
       </div>
         </div>
 
-      <div className="col-md-4 col-10 mx-auto">
+      <div className="col-md-4 col-10 mx-auto cards">
         <div class="card">
         <img src="https://raw.githubusercontent.com/CLiz17/superfone-integ/main/Images/freshsales.png" class="card-img-top" />
          <div class="card-body">
@@ -120,7 +136,7 @@ const Integration = () => {
       </div>
         </div>
 
-    <div className="col-md-4 col-10 mx-auto">
+    <div className="col-md-4 col-10 mx-auto cards">
       <div class="card">
         <img src="https://raw.githubusercontent.com/CLiz17/superfone-integ/main/Images/pipedrive.png" class="card-img-top"/>
         <div class="card-body">
@@ -134,7 +150,7 @@ const Integration = () => {
     </div>
   </div>
 
-  <div className="col-md-4 col-10 mx-auto">
+  <div className="col-md-4 col-10 mx-auto cards">
       <div class="card">
         <img src="https://raw.githubusercontent.com/CLiz17/superfone-integ/main/Images/agilecrm.png" class="card-img-top"/>
         <div class="card-body">
@@ -148,7 +164,7 @@ const Integration = () => {
     </div>
   </div>
 
-  <div className="col-md-4 col-10 mx-auto">
+  <div className="col-md-4 col-10 mx-auto cards">
       <div class="card">
         <img src="https://raw.githubusercontent.com/CLiz17/superfone-integ/main/Images/odoo.png" class="card-img-top"/>
         <div class="card-body">
@@ -157,7 +173,6 @@ const Integration = () => {
                        With a one-click sync, your HubSpot information will appear in
                         Wingman (chosen fields) and information will be pushed to your
                          CRM.</p>
-
       </div>
     </div>
   </div>
@@ -166,7 +181,7 @@ const Integration = () => {
   </div>
   </div>  
   </div>
-</div> 
+</div>
 </div>
 
   <div class="card text-center banner">
@@ -180,21 +195,25 @@ const Integration = () => {
                  </div>
   </div>
 </div>
-<div class="row">
-    <div class="column">
-      <img src="https://raw.githubusercontent.com/CLiz17/superfone-integ/main/src/components/head.png"/>   
+
+<div class="container1">
+  <div class="row justify-content-md-center">
+  </div>
+  <div class="row">
+    <div class="col">
+    <img src="https://raw.githubusercontent.com/CLiz17/superfone-integ/main/src/components/head.png"/>
     </div>
-    <div class="column"></div>
-    <div class="column">
-         
+    <div class="col-md-auto">
+      Pricing <br/> Integration <br/> Teams
     </div>
-    <div class="column">
-         
+    <div class="col-md-auto">
+      Blog <br/> Integration 
     </div>
-    <div class="column">
-    <img src="https://raw.githubusercontent.com/CLiz17/superfone-integ/main/Images/getitongoogle.png" className="img-fluid animated"/>   
+    <div class="col col-lg-2">
+    <img src="https://raw.githubusercontent.com/CLiz17/superfone-integ/main/Images/getitongoogle.png" className="img-fluid animated"/>
     </div>
   </div>
+</div>
   <hr/>
   <div id="foot" class="card-footer text-muted" >
   Copyright ⓒ Superfone.ai, All Rights Reserved
